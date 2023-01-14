1e3ca9a0aaea4e840273c251a958b36352869221
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2023-01-14 13:55:11 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -10,9 +10,9 @@
 # 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
 
 rm -rf package/kernel/mac80211
-svn export https://github.com/mj22226/openwrt/branches/linux-6.2/package/kernel/mac80211 package/kernel/mac80211
-rm -rf package/kernel/ksmbd
-svn export https://github.com/mj22226/openwrt/branches/linux-6.2/package/kernel/ksmbd package/kernel/ksmbd
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
+rm -rf package/kernel/mt76
+svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mt76 package/kernel/mt76
 rm -rf package/kernel/rtl8821cu
 
 # alist
