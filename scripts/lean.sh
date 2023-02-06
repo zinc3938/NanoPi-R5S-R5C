41302f265ea0369cbdcd1b184da0dbffce0cf2dd
骷髅头 <74764072+DHDAXCW@users.noreply.github.com>
2023-02-06 12:32:52 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -29,8 +29,12 @@ pushd package/community
 
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+cd openwrt-passwall
+git checkout 4fd4bf8
+cd ../
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+git checkout 4fd4bf8
 
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
