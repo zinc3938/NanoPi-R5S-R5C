a54a946b06b7eedfa3da2b88cafe82a3025f9ee0
骷髅头 <lasstp5011@gmail.com>
2023-02-10 17:59:20 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -13,8 +13,8 @@
 rm -rf package/kernel/rtl8821cu
 rm -rf package/kernel/mac80211
 rm -rf package/kernel/mt76
-cp -f $GITHUB_WORKSPACE/mt76/Makefile package/kernel/mt76/Makefile
 rm -rf package/network/services/hostapd
+svn export https://github.com/DHDAXCW/NanoPi-R5S-R5C/trunk/mt76 package/kernel/mt76
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
 svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 
