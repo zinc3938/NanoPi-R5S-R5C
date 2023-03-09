33e768b4bc0d37ea52099b42ccc2d47e46cedb11
骷髅头 <lasstp5011@gmail.com>
2023-03-09 20:04:42 +0800

    Update lean.sh

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -17,6 +17,8 @@ rm -rf package/network/services/hostapd
 svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
 svn export https://github.com/DHDAXCW/LubanCat-LEDE/trunk/scripts/mt76 package/kernel/mt76
 svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
+rm -rf package/kernel/mac80211/Makefile
+cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
 
 # alist
 git clone https://github.com/sbwml/luci-app-alist package/alist
