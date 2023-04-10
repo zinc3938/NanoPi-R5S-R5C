d178379ed773e7028f81b63922738fd8c3a24073
ling <1042585959@qq.com>
2023-04-10 20:03:13 +0800

    更新配置

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -75,6 +75,7 @@ git clone --depth=1 https://github.com/lisaac/luci-lib-docker
 # Add iStoreOS
 # git clone --depth=1 https://github.com/linkease/nas-packages
 # git clone --depth=1 https://github.com/linkease/nas-packages-luci
+git clone --depth=1 https://github.com/Siriling/nas-packages-luci.git
 
 # Add luci-theme
 git clone https://github.com/DHDAXCW/theme
