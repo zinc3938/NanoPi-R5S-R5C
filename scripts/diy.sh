5c6307167689a47e6860269ffc13b402277075e0
ling <1042585959@qq.com>
2023-05-04 19:02:18 +0800

    更新代码

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -134,11 +134,12 @@ sed -i 's/OpenWrt/R5S/g' package/base-files/files/bin/config_generate
 # sed -i 's/OpenWrt/R5S/g' package/lean/default-settings/files/zzz-default-settings
 
 # 系统信息内添加编译者信息
-sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><\/table>' package/lean/autocore/files/x86/index.htm
-sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><\/table>' package/lean/autocore/files/arm/index.htm
+sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><%:Resources link%><\/td><td><a target="blank" href="https://github.com/Siriling/NanoPi-R5S-R5C">固件仓库<\/a><\/td><\/tr><\/table>' package/lean/autocore/files/x86/index.htm
+sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><%:Resources link%><\/td><td><a target="blank" href="https://github.com/Siriling/NanoPi-R5S-R5C">固件仓库<\/a><\/td><\/tr><\/table>' package/lean/autocore/files/arm/index.htm
 
 # 给添加的代码添加汉化
 sed -i '$a\\nmsgid "Compiler author"\nmsgstr "编译作者"' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i '$a\\nmsgid "Resources link"\nmsgstr "资源链接"' feeds/luci/modules/luci-base/po/zh-cn/base.po
 
 # Test kernel 5.15
 sed -i 's/5.4/6.1/g' ./target/linux/rockchip/Makefile
