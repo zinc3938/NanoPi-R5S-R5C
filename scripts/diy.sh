c88ccfcb7b9f46ea0842dc082a1fbb883d403100
ling <1042585959@qq.com>
2023-05-05 14:41:51 +0800

    添加页面链接

--- scripts/diy.sh
+++ scripts/diy.sh
@@ -134,8 +134,10 @@ sed -i 's/OpenWrt/R5S/g' package/base-files/files/bin/config_generate
 # sed -i 's/OpenWrt/R5S/g' package/lean/default-settings/files/zzz-default-settings
 
 # 系统信息内添加编译者信息
-sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="blank" href="https://github.com/Siriling/NanoPi-R5S-R5C">固件仓库<\/a><\/td><td><a target="blank" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/x86/index.htm
-sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="blank" href="https://github.com/Siriling/NanoPi-R5S-R5C">固件仓库<\/a><\/td><td><a target="blank" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/arm/index.htm
+sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="blank" style="padding-right: 20px;" href="https://github.com/Siriling/NanoPi-R5S-R5C">固件仓库<\/a><a target="blank" style="padding-left: 20px;padding-right: 20px;" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/x86/index.htm
+sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="blank" style="padding-right: 20px;" href="https://github.com/Siriling/NanoPi-R5S-R5C">固件仓库<\/a><a target="blank" style="padding-left: 20px;padding-right: 20px;" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/arm/index.htm
+
+style="padding-right: 21px;padding-left: 21px;"
 
 # 给添加的代码添加汉化
 sed -i '$a\\nmsgid "Compiler author"\nmsgstr "编译作者"' feeds/luci/modules/luci-base/po/zh-cn/base.po
