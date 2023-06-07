b27748fa10bd99180635f6dde2616f205ab34fa9
ling <1042585959@qq.com>
2023-06-07 15:39:03 +0800

    更新脚本

--- scripts/full.sh
+++ scripts/full.sh
@@ -10,11 +10,6 @@
 # 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
 
 # mt7921 mt7916
-rm -rf package/kernel/mac80211
-rm -rf package/kernel/rtl8821cu
-rm -rf package/network/services/hostapd
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 rm -rf package/kernel/mac80211/Makefile
 cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
 
@@ -142,8 +137,5 @@ sed -i '$a\\nmsgid "Resources link"\nmsgstr "资源链接"' feeds/luci/modules/l
 rm -rf customfeeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 cp -f $GITHUB_WORKSPACE/images/bg1.jpg customfeeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
-# Test kernel 5.15
-sed -i 's/5.4/6.1/g' ./target/linux/rockchip/Makefile
-rm -rf target/linux/rockchip/image/armv8.mk
-cp -f $GITHUB_WORKSPACE/armv8.mk target/linux/rockchip/image/armv8.mk
+# pcie patch
 cp -f $GITHUB_WORKSPACE/999-fuck-rockchip-pcie.patch target/linux/rockchip/patches-6.1/999-fuck-rockchip-pcie.patch
--- scripts/lean.sh
+++ scripts/lean.sh
@@ -10,11 +10,6 @@
 # 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
 
 # mt7921 mt7916
-rm -rf package/kernel/mac80211
-rm -rf package/kernel/rtl8821cu
-rm -rf package/network/services/hostapd
-svn export https://github.com/openwrt/openwrt/trunk/package/kernel/mac80211 package/kernel/mac80211
-svn export https://github.com/openwrt/openwrt/trunk/package/network/services/hostapd package/network/services/hostapd
 rm -rf package/kernel/mac80211/Makefile
 cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
 
@@ -119,8 +114,5 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 
-# Test kernel 5.15
-sed -i 's/5.4/6.1/g' ./target/linux/rockchip/Makefile
-rm -rf target/linux/rockchip/image/armv8.mk
-cp -f $GITHUB_WORKSPACE/armv8.mk target/linux/rockchip/image/armv8.mk
+# pcie patch
 cp -f $GITHUB_WORKSPACE/999-fuck-rockchip-pcie.patch target/linux/rockchip/patches-6.1/999-fuck-rockchip-pcie.patch
