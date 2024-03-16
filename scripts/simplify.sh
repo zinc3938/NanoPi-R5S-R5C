bc545dd09e3c37de7c75a8445fbdec05f2407228
ling <1042585959@qq.com>
2024-03-16 14:06:24 +0800

    更新配置

--- scripts/simplify.sh
+++ scripts/simplify.sh
@@ -8,45 +8,83 @@
 #=================================================
 # Clone community packages to package/community
 
+# 添加第三方应用
+mkdir kenzok8
+pushd kenzok8
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages .
+popd
+
+mkdir kenzok8-small
+pushd kenzok8-small
+git clone --depth=1 https://github.com/kenzok8/small-package .
+popd
+
+mkdir kenzok8-outside
+pushd kenzok8-outside
+git clone --depth=1 https://github.com/kenzok8/small .
+popd
+
+mkdir Modem-Support
+pushd Modem-Support
+git clone --depth=1 https://github.com/Siriling/5G-Modem-Support .
+popd
+
+mkdir MyConfig
+pushd MyConfig
+git clone --depth=1 https://github.com/Siriling/OpenWRT-MyConfig .
+popd
+
 # Add application
 mkdir package/community
 pushd package/community
-
 # 系统相关应用
 #Poweroff
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-poweroff
+mkdir luci-app-poweroff
+cp -rf ../../kenzok8/luci-app-poweroff/* luci-app-poweroff
 #fileassistant
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-fileassistant
+mkdir luci-app-fileassistant
+cp -rf ../../kenzok8/luci-app-fileassistant/* luci-app-fileassistant
 #guest-wifi
 rm -rf ../../customfeeds/luci/applications/luci-app-guest-wifi
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-guest-wifi
+mkdir luci-app-guest-wifi
+cp -rf ../../kenzok8/luci-app-guest-wifi/* luci-app-guest-wifi
 #Onliner (need luci-app-nlbwmon)
-svn export https://github.com/rufengsuixing/luci-app-onliner/trunk luci-app-onliner
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 #Eqos
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-eqos
+mkdir luci-app-eqos
+cp -rf ../../kenzok8-small/luci-app-eqos/* luci-app-eqos
 #Wolplus
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-wolplus
+mkdir luci-app-wolplus
+cp -rf ../../kenzok8-small/luci-app-wolplus/* luci-app-wolplus
 
 # 存储相关应用
-svn export https://github.com/immortalwrt/luci/trunk/applications/luci-app-gowebdav
+mkdir gowebdav
+mkdir luci-app-gowebdav
+cp -rf ../../kenzok8-small/gowebdav/* gowebdav
+cp -rf ../../kenzok8-small/luci-app-wolplus/* luci-app-gowebdav
 
 # 科学上网和代理应用
 #VSSR（Hello Word）
 # svn export https://github.com/jerrykuku/lua-maxminddb/trunk lua-maxminddb
 # svn export https://github.com/jerrykuku/luci-app-vssr/trunk luci-app-vssr
 #OpenClash
-svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/lede/general/applications/luci-app-openclash temp/luci-app-openclash
-cp -rf temp/luci-app-openclash/* luci-app-openclash
+mkdir luci-app-openclash
+cp -rf ../../kenzok8/luci-app-openclash/* luci-app-openclash
+cp -rf ../../MyConfig/configs/lede/general/applications/luci-app-openclash/* luci-app-openclash
+# Daed-next
+# git clone https://github.com/sbwml/luci-app-daed-next
+# Subconverter
+# git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
 
 # 去广告
 #ADGuardHome
 rm -rf ../../customfeeds/packages/utils/adguardhome
 rm -rf ../../customfeeds/luci/applications/luci-app-adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome
-svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/lede/general/applications/luci-app-adguardhome temp/luci-app-adguardhome
-cp -rf temp/luci-app-adguardhome/* luci-app-adguardhome
+mkdir adguardhome
+mkdir luci-app-adguardhome
+cp -rf ../../kenzok8/adguardhome/* adguardhome
+cp -rf ../../kenzok8/luci-app-adguardhome/* luci-app-adguardhome
+cp -rf ../../MyConfig/configs/lede/general/applications/luci-app-adguardhome/* luci-app-adguardhome
 sed -i 's/默认账号和密码均为：admin/默认用户名和密码均为root/' luci-app-adguardhome/po/zh-cn/AdGuardHome.po
 sed -i 's/网页管理账号和密码:admin ,端口:/端口/' luci-app-adguardhome/po/zh-cn/AdGuardHome.po
 #dnsfilter
@@ -80,39 +118,74 @@ sed -i 's/网页管理账号和密码:admin ,端口:/端口/' luci-app-adguardho
 # svn export https://github.com/kenzok8/small-package/trunk/luci-app-ddnsto
 
 # iStore应用
-svn export https://github.com/kenzok8/small-package/trunk/taskd
-svn export https://github.com/kenzok8/small-package/trunk/luci-lib-taskd
-svn export https://github.com/kenzok8/small-package/trunk/luci-lib-xterm
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-store
-svn export https://github.com/kenzok8/small-package/trunk/quickstart
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-quickstart
+mkdir taskd
+mkdir luci-lib-taskd
+mkdir luci-lib-xterm
+mkdir luci-app-store
+mkdir quickstart
+mkdir luci-app-quickstart
+cp -rf ../../kenzok8-small/taskd/* taskd
+cp -rf ../../kenzok8-small/luci-lib-taskd/* luci-lib-taskd
+cp -rf ../../kenzok8-small/luci-app-xterm/* luci-app-xterm
+cp -rf ../../kenzok8-small/luci-app-store/* luci-app-store
+cp -rf ../../kenzok8-small/quickstart/* quickstart
+cp -rf ../../kenzok8-small/luci-app-quickstart/* luci-app-quickstart
 
 # 其他
 #Socat
 rm -rf ../../customfeeds/luci/applications/luci-app-socat
-svn export https://github.com/kenzok8/small-package/trunk/luci-app-socat
+mkdir luci-app-socat
+cp -rf ../../kenzok8-small/luci-app-socat/* luci-app-socat
 #Unblockneteasemusic
+rm -rf ../../customfeeds/luci/applications/Unblockneteasemusic
 rm -rf ../../customfeeds/luci/applications/luci-app-unblockmusic
-svn export https://github.com/kenzok8/openwrt-packages/trunk/UnblockNeteaseMusic
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-unblockneteasemusic
-#OpenAppFilter
-svn export https://github.com/destan19/OpenAppFilter/trunk OpenAppFilter
+mkdir UnblockNeteaseMusic
+mkdir luci-app-unblockneteasemusic
+cp -rf ../../kenzok8/UnblockNeteaseMusic/* UnblockNeteaseMusic
+cp -rf ../../kenzok8/luci-app-unblockneteasemusic/* luci-app-unblockneteasemusic
+# OpenAppFilter
+git clone --depth=1 https://github.com/destan19/OpenAppFilter
 
 # 网络接口
 #Minieap
-svn export https://github.com/kenzok8/small-package/trunk/luci-proto-minieap
-# svn export https://github.com/kenzok8/small-package/trunk/luci-app-minieap
-
-# Apk (Apk Packages Manager)
-svn export https://github.com/openwrt/packages/trunk/utils/apk
+mkdir luci-proto-minieap
+cp -rf ../../kenzok8-small/luci-proto-minieap/* luci-proto-minieap
+# cp -rf ../../kenzok8-small/luci-app-minieap/* luci-app-minieap
 
 # 主题
-git clone https://github.com/DHDAXCW/theme
-svn export https://github.com/jerrykuku/luci-theme-argon/branches/18.06 luci-theme-argon
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argon-config
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
 rm -rf ../../customfeeds/luci/themes/luci-theme-argon
-#rm -rf ./luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+rm -rf ../../customfeeds/luci/themes/luci-theme-argon-mod
+rm -rf ./luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 cp -f $GITHUB_WORKSPACE/images/bg1.jpg luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg # 修改默认背景
+git clone https://github.com/DHDAXCW/theme
+
+# 5G通信模组拨号工具
+rm -rf package/wwan/driver/quectel_QMI_WWAN
+rm -rf package/wwan/driver/quectel_MHI
+rm -rf package/wwan/app/quectel_cm_5G
+mkdir quectel_QMI_WWAN
+mkdir quectel_MHI
+mkdir quectel_cm_5G
+# mkdir luci-app-hypermodem
+cp -rf ../../Modem-Support/quectel_QMI_WWAN/* quectel_QMI_WWAN
+cp -rf ../../Modem-Support/quectel_MHI/* quectel_MHI
+cp -rf ../../Modem-Support/quectel_cm_5G/* quectel_cm_5G
+# cp -rf ../../Modem-Support/luci-app-hypermodem/* luci-app-hypermodem
+
+# 5G模组短信插件
+# rm -rf customfeeds/luci/applications/luci-app-sms-tool
+# mkdir luci-app-sms-tool
+# cp -rf ../../Modem-Support/luci-app-sms-tool/* luci-app-sms-tool
+# cp -rf ../../MyConfig/configs/istoreos/general/applications/luci-app-sms-tool/* luci-app-sms-tool
+
+#5G模组插件
+rm -rf customfeeds/package/utils/sms-tool
+mkdir sms-tool
+mkdir luci-app-modem
+cp -rf ../../Modem-Support/sms-tool/* sms-tool
+cp -rf ../../Modem-Support/luci-app-modem/* luci-app-modem
 
 popd
 
@@ -129,23 +202,6 @@ popd
 rm -rf package/kernel/mac80211/Makefile
 cp -f $GITHUB_WORKSPACE/Makefile package/kernel/mac80211/Makefile
 
-# 修复移远PCIe驱动(quectel_MHI)
-rm -rf package/wwan/driver/quectel_MHI
-svn export https://github.com/Siriling/5G-Modem-Support/trunk/quectel_MHI package/wwan/driver/quectel_MHI
-
-#修复IPV6下发问题
-rm -rf customfeeds/luci/applications/luci-app-hypermodem
-svn export https://github.com/Siriling/5G-Modem-Support/trunk/luci-app-hypermodem package/community/luci-app-hypermodem
-chmod -R a+x package/community/luci-app-hypermodem/root/etc/init.d/*
-
-# 5G模组短信插件
-rm -rf customfeeds/package/utils/sms-tool
-rm -rf customfeeds/luci/applications/luci-app-sms-tool
-svn export https://github.com/dwj0/luci-app-sms-tool/trunk/sms-tool package/community/sms-tool
-svn export https://github.com/dwj0/luci-app-sms-tool/trunk/luci-app-sms-tool package/community/luci-app-sms-tool
-svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/lede/general/applications/luci-app-sms-tool temp/luci-app-sms-tool
-cp -rf temp/luci-app-sms-tool/* package/community/luci-app-sms-tool
-
 # 5G模组拨号脚本
 mkdir -p package/base-files/files/root/5GModem
 cp -rf $GITHUB_WORKSPACE/tools/5G模组拨号脚本/5GModem/* package/base-files/files/root/5GModem
@@ -155,12 +211,18 @@ svn export https://github.com/Siriling/OpenWRT-MyConfig/trunk/configs/lede/gener
 # 修改默认IP地址
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
+# 修改子网掩码
+#sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
+
 # 修改主机名字
 sed -i 's/OpenWrt/R5S/g' package/base-files/files/bin/config_generate
 
 # 修改版本号
 # sed -i 's/R22.3.3/R22.3.3定制版/g' package/lean/default-settings/files/zzz-default-settings
 # sed -i 's/OpenWrt/R5S/g' package/lean/default-settings/files/zzz-default-settings
+
+# 默认打开WiFi
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 系统信息内添加编译者信息
 sed -i '/<tr><td width="33%"><%:CPU usage (%)%><\/td><td id="cpuusage">-<\/td><\/tr>/a\<tr><td width="33%"><%:Compiler author%><\/td><td>Siriling<\/td><\/tr><tr><td width="33%"><%:Resources link%><\/td><td><a target="_blank" style="padding-right: 20px;" href="https://github.com/Siriling/NanoPi-R5S-R5C">固件仓库<\/a><a target="_blank" style="padding-left: 20px;padding-right: 20px;" href="https://blog.siriling.com:1212/2023/03/18/openwrt-5g-modem">模组教程<\/a><\/td><\/tr>' package/lean/autocore/files/x86/index.htm
